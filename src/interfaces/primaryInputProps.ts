c29fb803a734bf94f19ead8c1797b50df099bd56
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-08-22 22:04:01 -0300

    refact: input types

--- src/interfaces/primaryInputProps.ts
+++ src/interfaces/primaryInputProps.ts
@@ -1,6 +1,14 @@
-export interface PrimaryInput {
+export interface PrimaryInputType {
     nome: string,
     email: string,
-    cpf: number,
-    nascimento: Date
+    cpf: number | string,
+    nascimento: Date,
+    endereco: Endereco
+}
+
+interface Endereco {
+    estado: string,
+    cidade: string,
+    rua: string,
+    numero: string | number
 }
